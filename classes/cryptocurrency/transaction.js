901b1df60266b6e8e91b6acc7f4658f2e47bf001
Joseph-Emmanuel <josephE.banzio@gmail.com>
2020-05-14 01:30:46 +0800

    optimizing transactions

--- classes/cryptocurrency/transaction.js
+++ classes/cryptocurrency/transaction.js
@@ -11,7 +11,7 @@ class Transaction {
         const transaction = new this()
 
         if (amount > senderWallet.balance){
-            console.error(`Amount: ${amount} exceeds balance.`)
+            console.log(`Amount: ${amount} exceeds balance.`)
             return;  
         }
 
@@ -25,6 +25,21 @@ class Transaction {
         return transaction
     }
 
+    updateTransaction(senderWallet, recipient, amount){
+        const senderOutput = this.outputs.find(output => output.address === senderWallet.publicKey)
+        
+        if(amount > senderOutput.amount){
+            console.log(`Amount: ${amount} exceeds balance.`)
+            return; 
+        }
+
+        senderOutput.amount = senderOutput.amount - amount
+        this.outputs.push({amount, address: recipient});
+        Transaction.signTransaction(this, senderWallet)
+
+        return this;
+    }
+
     static signTransaction(transaction, senderWallet){
         transaction.input = {
             timestamp: Date.now(),
@@ -33,6 +48,12 @@ class Transaction {
             signature: senderWallet.sign(ChainUtil.hash(transaction.outputs))
         }
     }
+
+    static verifyTransaction(transaction){
+        return ChainUtil.verifySignature(transaction.input.address, transaction.input.signature, ChainUtil.hash(transaction.outputs))
+    }
+
+
 }
 
 module.exports = Transaction
--- tests/transaction.test.js
+++ tests/transaction.test.js
@@ -22,6 +22,19 @@ describe('Transaction', () => {
         .toEqual(amount)
     })
 
+    it('Inputing the balance of the wallet', () => {
+        expect(transaction.input.amount).toEqual(wallet.balance)
+    })
+
+    it('Validating a transaction', () => {
+        expect(Transaction.verifyTransaction(transaction)).toBe(true)
+    })
+
+    it('Invalidating a transaction', () => {
+        transaction.outputs[0].amount = 10000;
+        expect(Transaction.verifyTransaction(transaction)).toBe(false)
+    })
+
     describe('Transacting with an amount that exceeds the balance', () => {
         beforeEach(() => {
             amount = 5000
@@ -33,6 +46,24 @@ describe('Transaction', () => {
             expect(transaction).toEqual(undefined)
         })
 
+    })
+
+    describe('Updating a transaction', () => {
+        let next_amount, next_recipient
+
+        beforeEach(() => {
+            next_amount = 20
+            next_recipient = 'n3xt-4ddr355'
+            transaction = transaction.updateTransaction(wallet, next_recipient, next_amount)
+        })
+
+        it('Substracting the next amount from the sender\'s output', () => {
+            expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount).toEqual(wallet.balance - amount - next_amount)
+        })
+
+        it('Outputing an amount for the next recipient', () => {
+            expect(transaction.outputs.find(output => output.address === next_recipient).amount).toEqual(next_amount)
+        })
     })
     
 })
